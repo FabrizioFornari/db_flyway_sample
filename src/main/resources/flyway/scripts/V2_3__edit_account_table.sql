eaaf0ba4af37fe2d4d3aaf4634d1af35e28c4b20
Fabrizio <fabrizio.fornari@unicam.it>
2021-12-14 21:39:12 +0100

    added few scripts to simulate schema changes

---
+++ src/main/resources/flyway/scripts/V2_3__edit_account_table.sql
@@ -0,0 +1,2 @@
+ALTER TABLE flyway_accounts
+ADD COLUMN actual_position VARCHAR (255);
---
+++ src/main/resources/flyway/scripts/add_user.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.flyway_accounts(
+	user_id, username, password, email, created_on, last_login)
+	VALUES (1, 'firstuser', 123456, 'firstuser@unicam.it', '2004-10-19 10:23:54+02' , '2004-10-19 10:23:54+02');
